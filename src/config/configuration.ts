e7847f764c5d63ad8ff16bbee505455505960708
EAEzequielAlonso <eze.g.alonso@gmail.com>
2026-09-04 18:53:42 -0300

    sincrinze false

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -18,7 +18,7 @@ export const getDatabaseConfig = (): TypeOrmModuleOptions => ({
   ],
   autoLoadEntities: true,
   logging: false, // Set to true to debug queries
-  synchronize: true,
+  //synchronize: true,
   //dropSchema: true,
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsRun: false,
